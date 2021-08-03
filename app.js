99f390fc2be54c8a806214d658a014cba8eec150
maurizio <mauriziodm91@gmail.com>
2021-08-03 20:04:31 -0300

    modified styles file and added a spinner to the app

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-document.querySelector('#loan-form').addEventListener('submit', calculateResults);
+document.querySelector('#loan-form').addEventListener('submit', function(e){
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'block';
+    setTimeout(calculateResults, 2000);
 
-function calculateResults(e){
+    
+    e.preventDefault();
+});
+
+function calculateResults(){
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
     const years = document.getElementById('years');
@@ -19,14 +26,17 @@ function calculateResults(e){
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly*calculatedPayments)-principal).toFixed(2);
+        document.getElementById('results').style.display = 'block';
+        document.getElementById('loading').style.display = 'none';
     } else {
         showError('por favor verifique los numeros');
     }
-    e.preventDefault();
 }
 
 
 function showError(error){
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'none';
     const errorDiv = document.createElement('div');
     const card = document.querySelector('.card');
     const heading = document.querySelector('.heading');
